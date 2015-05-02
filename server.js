ddb0c32a260a2bc82aff16c9e223bbbc8ba97fc1
nirma <nirmsk@hotmail.com>
2015-05-01 19:34:59 -0700

    commit from client to websites

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Code with GitHub!</h1>');
+    res.end('<h1>Code with GitHub from client!</h1>');
 }).listen(process.env.PORT);  
